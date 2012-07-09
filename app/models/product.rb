268b1b298191e8496d4dcdcc887b9ac74cb8da59
Anerhan <develby@gmail.com>
2012-07-10 02:57:45 +0300

    Add Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,20 @@ class Product < ActiveRecord::Base
   validates :price,       :presence => true, :numericality=>true, :inclusion => {:in => 0..10000}
   validates :code, :name, :presence => true
 
+
+  validate :price_must_be_greather_than_discount, :unless => Proc.new { |p| p.tariff_rule.blank? }
+
+
+
+
+
+  private
+
+
+  def price_must_be_greather_than_discount
+    errors.add(:price,:invalid) if price < tariff_rule.discount
+  end
+
+
+
 end
