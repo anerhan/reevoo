2dab125cee54b7823ddb659dfde52fedf5d44a94
Anerhan <develby@gmail.com>
2012-07-13 16:43:20 +0300

    Add New Method

--- app/models/tariff_rule.rb
+++ app/models/tariff_rule.rb
@@ -8,8 +8,8 @@ class TariffRule < ActiveRecord::Base
 
   validates :name, :presence => true
   validates :discount, :presence => true, :numericality => true, :inclusion => {:in => 0..10000}
-  validates :quantity, :schema, :presence => true, :numericality => {:only_integer=>true}
-  validates :schema,   :numericality => true, :inclusion => {:in => 0..(SCHEMAS.size - 1)}
+  validates :quantity, :schema, :presence => true, :numericality => {:only_integer => true}
+  validates :schema, :numericality => true, :inclusion => {:in => 0..(SCHEMAS.size - 1)}
 
 
   # Returns index by Schema Name
@@ -19,13 +19,13 @@ class TariffRule < ActiveRecord::Base
 
   #Apply discount for ProductsLists Object
   def apply(products)
-    rule_products = products.select{ |p| p.tariff_rule_id == id }
+    rule_products = products.select { |p| p.tariff_rule_id == id }
     full_price    = rule_products.collect { |p| p.price }.sum
     @rp_count     = rule_products.size
     @dc           = 0
 
     if quantity == 0
-      @dc = rp_count * discount
+      every
     elsif quantity > 0
       send(SCHEMAS[schema]) if @rp_count >= quantity
     end
@@ -35,8 +35,12 @@ class TariffRule < ActiveRecord::Base
 
   private
 
+  def every
+    @dc = @rp_count * discount
+  end
+
   def every_free
-    @dc =  (@rp_count / quantity) * discount
+    @dc = (@rp_count / quantity) * discount
   end
 
   def for_all
